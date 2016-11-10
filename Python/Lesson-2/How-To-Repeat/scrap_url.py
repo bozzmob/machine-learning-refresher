c4c1e7e3ff45f556400533f4b8399e0dbb915a27
Satvik Jagannath <satvik.jagannath7@gmail.com>
2016-11-10 21:02:46 +0530

    Scrap all URLs - Complete

--- Python/Lesson-2/How-To-Repeat/scrap_url.py
+++ Python/Lesson-2/How-To-Repeat/scrap_url.py
@@ -11,13 +11,22 @@
 
 def get_next_target(page):
     start_link = page.find('<a href=')
-
     #Insert your code below here
     if start_link == -1:
         return None, 0
-    else:
-        start_quote = page.find('"', start_link)
-        end_quote = page.find('"', start_quote + 1)
-        url = page[start_quote + 1:end_quote]
-    
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
     return url, end_quote
+
+def print_all_links(page):
+	while True:
+		url, endpos = get_next_target(page)
+		if url:
+			print url
+			page = page[endpos:]
+		else:
+			break
+
+print_all_links('this is <a href="http://google.com">Google</a> and <a href="http://yahoo.com">Yahoo</a>!!')
+
