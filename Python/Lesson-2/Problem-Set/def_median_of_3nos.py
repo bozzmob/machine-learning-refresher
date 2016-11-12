e1d4856dd6c4a7e599042c258eef2d7a05c6552c
Satvik Jagannath <satvik.jagannath7@gmail.com>
2016-11-12 10:26:31 +0530

    def_median : WIP

---
+++ Python/Lesson-2/Problem-Set/def_median_of_3nos.py
@@ -0,0 +1,39 @@
+# Define a procedure, median, that takes three
+# numbers as its inputs, and returns the median
+# of the three numbers.
+
+# Make sure your procedure has a return statement.
+
+def bigger(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+def biggest(a,b,c):
+    return bigger(a,bigger(b,c))
+
+def median(a,b,c):
+    if bigger(a,b):
+        if bigger(a,c):
+            if bigger(b,c):
+                return b
+            else:
+                return c
+        else:
+            return a
+    else:
+        if bigger(b,c):
+            return c
+        else:
+            return b
+
+
+#print(median(1,2,3))
+#>>> 2
+
+#print(median(9,3,6))
+#>>> 6
+
+#print(median(7,8,7))
+#>>> 7
